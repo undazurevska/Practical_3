8f923fa69d52f6778148ee61e868fc6320dee5ed
svitkadiii <95414945+svitkadiii@users.noreply.github.com>
2021-12-08 13:50:26 +0200

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,1 +1,28 @@
-urgruegfhjkdhjfkhda
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+import { useState } from 'react';
+
+export default function HomeScreen(
+  {navigation}) {
+    const [counter, setCounter] = useState(0);
+    return (
+  );
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'top',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+    margin: 10,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
